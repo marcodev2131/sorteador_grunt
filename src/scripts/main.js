27f7529931379c055c8c5914f896106b27c6a32e
marcodev2131 <145700074+marcodev2131@users.noreply.github.com>
2024-03-27 21:01:32 -0300

    Atualizacao sorteador

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,6 +8,6 @@ document.addEventListener('DOMContentLoaded', function() {
         numeroAleatorio = Math.floor(numeroAleatorio + 1);
 
         document.getElementById('resultado-valor').innerText = numeroAleatorio;
-        document.querySelector('.resultado').computedStyleMap.display = 'block';
+        document.querySelector('.resultado').style.display = 'block';
     })
 })
